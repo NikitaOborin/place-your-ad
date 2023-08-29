5f79cc990085f3767fbb0f6a9a8f3c63157754f0
Oborin Nikita <oborinikita@yandex.ru>
2023-08-29 22:59:44 +0700

    Initial commit

---
+++ src/main/java/com/example/placeyourad/PlaceYourAdApplication.java
@@ -0,0 +1,13 @@
+package com.example.placeyourad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaceYourAdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaceYourAdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/placeyourad/PlaceYourAdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.placeyourad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaceYourAdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
